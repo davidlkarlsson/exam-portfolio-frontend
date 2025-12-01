fdff456c8cb7d385833e0acd23fb82cbd58693f2
David Karlsson <davidfkarlsson@proton.me>
2025-12-01 19:28:59 +0100

    feat(about): implement centered two-column layout with profile image, description, info cards and tools section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'; // Makes the component a Client Component (interactive with useState e.g)
 
+
+import { About } from "./_components/about/about.component";
 import { Header } from "./_components/header/header.component";
 import { Navbar } from "./_components/navbar/navbar.component";
 
@@ -11,7 +13,7 @@ export default function Home() {
 
     <Navbar />
     <Header />
-    
+    <About />
     </>
     
   );
